9fd087eb7862d6fd300ec7ab0092a3b9cac0bc20
Micha Janssen <f2pview@gmail.com>
2012-09-14 16:04:42 +0200

    Added Name and Dimensions
    Added the variables and dimensions of the JFrame, Also Set it to not be
    resizable(just annoying) and Relative to nothing and ofcourse set it if
    we exit the frame that the program closes on its own

--- TowerDefence/Frame.java
+++ TowerDefence/Frame.java
@@ -1,9 +1,18 @@
 package TowerDefence;
 import javax.swing.*;
+import java.awt.*;
 public class Frame extends JFrame{
+	//Variables
+	public static String title = "SuperTowerStroll InvDev 1.0"; 
+	public static Dimension size = new Dimension(550,750);
+	
 	//Constructor
 	public void Frame(){
-		
+		setTitle(title);
+		setSize(size);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	//Applet Importation Compatibility
 	public void init(){
